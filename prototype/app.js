f73c924be03629816205cb8a6b69158639224927
felix-rojas <A01201946@tec.mx>
2022-08-26 17:42:34 -0500

    add dependencies to package.json, folder and file structuring for MVC

---
+++ prototype/app.js
@@ -0,0 +1,20 @@
+// dependencias
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const path = require("path");
+// express maneja rutas
+const express = require("express");
+const app = express();
+// view engine es ejs, views es views por default
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs", { foo: "FOO" });
+});
+
+const rutas_natdev = require("./routes/natdev.routes");
+app.use("/home", rutas_natdev);
+
+app.listen(4000, () => console.log("Example app listening on port 4000!"));
---
+++ prototype/routes/natdev.routes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const path = require('path');
+
+const router = express.Router();
+
+// Route handling
+
+module.exports = router;
