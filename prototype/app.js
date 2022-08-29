2e39ca827e77ed3425acef0ceaef781e3a2fe75b
felix-rojas <A01201946@tec.mx>
2022-08-29 16:23:06 -0500

    add auth0 to app

--- prototype/app.js
+++ prototype/app.js
@@ -10,11 +10,35 @@ app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-    res.render("actividades.ejs", { foo: "FOO" });
-});
+const { auth } = require("express-openid-connect");
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: "a long, randomly-generated string stored in env",
+  baseURL: "http://localhost:4000",
+  clientID: "atG2tlYdfbLvnTyWJHqSFx4fCkbtmani",
+  issuerBaseURL: "https://dev-fx-x8vee.us.auth0.com",
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
 
 const rutas_natdev = require("./routes/natdev.routes");
 app.use("/home", rutas_natdev);
+
+// User profile page
+const { requiresAuth } = require('express-openid-connect');
+
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+});
+
+// req.isAuthenticated is provided from the auth router
+app.get("/", (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? "Logged in" : "Logged out");
+});
+
 
 app.listen(4000, () => console.log("http://localhost:4000/"));
