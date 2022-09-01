89e588e67da527a7168224fbf2d4a5e6b6a250f6
felix-rojas <A01201946@tec.mx>
2022-09-01 17:02:26 -0500

    genera tablas con PK's

--- prototype/sql/database.sql
+++ prototype/sql/database.sql
@@ -1,10 +1,12 @@
 -- Database name
+-- Borra la anterior
+DROP DATABASE `starDB`;
+-- Crea nueva base de datos
 CREATE DATABASE `starDB`;
-SELECT DATABASE `starDB`;
 
 -- Tabla Empleado
 CREATE TABLE `empleado` (
-    `idEmpleado` int(11) NOT NULL,
+    `id_empleado` int(11) NOT NULL,
     `nombre` varchar(45) COLLATE utf8_spanish2_ci NOT NULL,
     `tipoTurno` TINYINT(1) NOT NULL,
     `correo` varchar(60) COLLATE utf8_spanish2_ci NOT NULL,
@@ -14,14 +16,14 @@ CREATE TABLE `empleado` (
 -- Tabla actividad
 CREATE TABLE `actividad` (
     `numHoras` float NOT NULL,
-    `descripcionActividad` varchar(255) COLLATE utf8_spanish2_ci NOT NULL,
+    `descripcion_actividad` varchar(255) COLLATE utf8_spanish2_ci NOT NULL,
     `fecha` date NOT NULL,
-    `idProyecto` int(11)
+    `id_actividad` int(11)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
 -- Tabla proyecto
 CREATE TABLE `proyecto` (
-    `idProyecto` int(11) NOT NULL,
+    `id_proyecto` int(11) NOT NULL,
     `horas_registradas` float,
     `nombre_proyecto` varchar(45) COLLATE utf8_spanish2_ci NOT NULL,
     `descripcion_proyecto` varchar(255) COLLATE utf8_spanish2_ci NOT NULL,
@@ -32,7 +34,8 @@ CREATE TABLE `proyecto` (
 CREATE TABLE `registra` (
     `numHoras` float NOT NULL,
     `descripcion_actividad` varchar(255) COLLATE utf8_spanish2_ci NOT NULL,
-    `fecha` date NOT NULL
+    `fecha` date NOT NULL,
+    `correo` varchar(60) COLLATE utf8_spanish2_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
 -- Tabla catalogo_variable 
@@ -42,7 +45,7 @@ CREATE TABLE `catalogo_variable` (
     `horas_tiempo_completo` float NOT NULL,
     `horas_tiempo_medio` float NOT NULL,
     `coeficiente_efectividad` float NOT NULL,
-    `fecha` date NOT NULL,
+    `fecha` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
 -- Tabla reporte 
@@ -53,7 +56,7 @@ CREATE TABLE `reporte` (
     `capacidad_utilizada_ajustada` float NOT NULL,
     `horas_tiempo_medio` int NOT NULL,
     `coeficiente_efectividad` float NOT NULL,
-    `fecha` date NOT NULL,
+    `fecha` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
 -- Llaves primarias 
@@ -69,7 +72,7 @@ ALTER TABLE `proyecto`
 ALTER TABLE `actividad`
     ADD PRIMARY KEY (`fecha`, `id_actividad`);
 
-ALTER TABLE `catalogo_variables`
+ALTER TABLE `catalogo_variable`
     ADD PRIMARY KEY (`fecha`);
 
 ALTER TABLE `reporte`
