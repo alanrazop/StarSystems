d6e98f007d87a56afd0659d98d39288be3b28fcc
Alan Razo <a01703350@itesm.mx>
2022-09-07 13:04:58 -0500

    Se agregaron vistas, controladores y rutas basicas para proyectos, colab, reportes

--- prototype/routes/natdev.routes.js
+++ prototype/routes/natdev.routes.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const path = require('path');
+const actController = require('../controllers/actividad.controller');
+const projectsController = require('../controllers/proyecto.controller');
+const colabController = require('../controllers/colaborador.controller');
+const reporteController = require('../controllers/reporte.controller');
 
 const router = express.Router();
 
 // Route handling
+router.get('/tareas', actController.getActividad);
 
+router.get('/proyectos', projectsController.getProyecto);
+
+router.get('/colaboradores', colabController.getColaborador);
+
+router.get('/reportes', reporteController.getReportes);
 
 module.exports = router;
