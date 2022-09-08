3ffab95f61fd534b7ee293f8bb42c4dbba9e5ce9
Alan Razo <a01703350@itesm.mx>
2022-09-07 23:56:00 -0500

    Vistas mejoradas y se agrego card de colab

--- prototype/app.js
+++ prototype/app.js
@@ -9,6 +9,8 @@ const app = express();
 app.set("view engine", "ejs");
 app.set('views', 'views');
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
