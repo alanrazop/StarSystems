26a95a8f1731d384be6216c8c96ab21914ac064c
MonseMoreno <a01276775@tec.mx>
2022-09-13 18:19:24 -0500

    actiidades.models

---
+++ prototype/models/actividades.model.js
@@ -0,0 +1,17 @@
+const db = require('../util/database');
+
+module.exports = class Actividades {
+    //constructor(){
+    // }
+    static fetchAll() {
+        return db.execute('SELECT * FROM actividad a, registra r, proyecto p, empleado e WHERE a.id_actividad = r.id_actividad and a.id_proyecto = p.id_proyecto and r.id_empleado = e.id_empleado   ');
+    }
+
+    // static fetchVerProyectoAct() {
+    //     return db.execute('SELECT * FROM actividad a, proyecto p WHERE a.id_proyecto = p.id_proyecto');
+    // }
+
+    // static fetchVerEmpleadoAct() {
+    //     return db.execute('SELECT * FROM actividad a, proyecto p WHERE a.id_proyecto = p.id_proyecto');
+    // }
+}
