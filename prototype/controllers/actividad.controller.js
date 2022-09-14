4e3fe994aea01f87868afca25d31624eb10fdd26
Monse Casas <a01276775@tec.mx>
2022-09-14 10:06:31 -0500

    Dropdown de empleados en agregar tarea

--- prototype/controllers/actividad.controller.js
+++ prototype/controllers/actividad.controller.js
@@ -1,22 +1,31 @@
 const path = require('path');
 const Actividades = require('../models/actividades.model');
 const Proyectos = require('../models/proyectos.model');
+const Empleados = require('../models/empleados.model');
+
+
 
 exports.getActividad  = (request, response, next) => {
     Actividades.fetchAll()
-        .then(([rows, fieldData]) => {
+    .then(([rows, fieldData]) => {
+        Empleados.fetchAll()
+        .then(([empleados,fieldData]) =>{
             Proyectos.fetchAll()
-                .then(([proyectos, fieldData]) => {
-                    response.render(path.join('actividades.ejs'), {
-                        actividades: rows,
-                        proyectos: proyectos,
-                    })
-                }).catch(error => {
-                    console.log(error);
-                });
-
-        })
-        .catch(err => console.log(err));
-
-
+            .then(([proyectos,fieldData]) =>{
+                response.render(path.join('actividades.ejs'), {
+                    actividades: rows,
+                    empleados:empleados,
+                    proyectos: proyectos
+                })
+            }).catch(error => {
+                console.log(error);
+            });
+        }).catch(error =>{
+            console.log(error);
+        });
+       
+    })
+    .catch(err => console.log(err)); 
+  
+        
 }
