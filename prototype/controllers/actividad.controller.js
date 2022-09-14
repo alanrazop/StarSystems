5d3e22285f3d68d6965bc526c3ebb6da399e0c5e
Monse Casas <a01276775@tec.mx>
2022-09-14 09:15:48 -0500

    Mostrar la lista de proyectos en agregar actividad

--- prototype/controllers/actividad.controller.js
+++ prototype/controllers/actividad.controller.js
@@ -1,14 +1,22 @@
 const path = require('path');
 const Actividades = require('../models/actividades.model');
+const Proyectos = require('../models/proyectos.model');
+
+exports.getActividad  = (request, response, next) => {
+    Actividades.fetchAll()
+        .then(([rows, fieldData]) => {
+            Proyectos.fetchAll()
+                .then(([proyectos, fieldData]) => {
+                    response.render(path.join('actividades.ejs'), {
+                        actividades: rows,
+                        proyectos: proyectos,
+                    })
+                }).catch(error => {
+                    console.log(error);
+                });
+
+        })
+        .catch(err => console.log(err));
 
 
-exports.getActividad = (request, response, next) => {
-        Actividades.fetchAll()
-        .then(([rows,fieldData]) => {
-            response.render(path.join('actividades.ejs'), {
-                actividades: rows,
-            })
-        }).catch(error => {
-            console.log(error);
-        });
-};
+}
