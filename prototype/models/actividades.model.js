54727b12cb7d361379282d070af555036f31ed34
Alan Razo <a01703350@itesm.mx>
2022-09-13 20:13:56 -0500

    Fecha corregida

--- prototype/models/actividades.model.js
+++ prototype/models/actividades.model.js
@@ -4,7 +4,7 @@ module.exports = class Actividades {
     //constructor(){
     // }
     static fetchAll() {
-        return db.execute('SELECT * FROM actividad a, registra r, proyecto p, empleado e WHERE a.id_actividad = r.id_actividad and a.id_proyecto = p.id_proyecto and r.id_empleado = e.id_empleado   ');
+        return db.execute('SELECT * FROM actividad a, registra r, proyecto p, empleado e WHERE a.id_actividad = r.id_actividad and a.id_proyecto = p.id_proyecto and r.id_empleado = e.id_empleado');
     }
 
     // static fetchVerProyectoAct() {
