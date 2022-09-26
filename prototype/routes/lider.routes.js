1dc01056a68a331e74bbfe8b0b778a97a12e6b2a
felix-rojas <a01201946@tec.mx>
2022-09-26 18:48:17 -0500

    add lider router

---
+++ prototype/routes/lider.routes.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+const actController = require('../controllers/actividad.controller');
+const projectsController = require('../controllers/proyecto.controller');
+const colabController = require('../controllers/colaborador.controller');
+const reporteController = require('../controllers/reporte.controller');
+
+const { requiresAuth } = require('express-openid-connect');
+
+const router = express.Router();
+
+// Route handling
+router.get('/tareas', actController.getActividad);
+
+router.post('/tareas', actController.postActividad);
+
+router.get('/edit/:id', actController.getEditAct);
+
+router.post('/edit', actController.postEditAct);
+
+router.post('/delete', actController.postDeleteAct);
+
+router.get('/proyectos', projectsController.getProyecto);
+
+router.get('/colaboradores', colabController.getEmpleado);
+
+router.get('/colaboradores/buscar/:valor', colabController.getBuscarColab);
+
+module.exports = router;
