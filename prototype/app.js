fa0a0438f887d7c1cfaa610704c5b59149d990ce
felix-rojas <a01201946@tec.mx>
2022-09-27 11:48:03 -0500

    add env variables

--- prototype/app.js
+++ prototype/app.js
@@ -8,6 +8,11 @@ const app = express();
 const { auth } = require('express-openid-connect');
 const { requiresAuth } = require('express-openid-connect');
 
+// environment vars
+const authconfig = require('./auth0.env');
+
+console.log(authconfig);
+
 app.get('/profile', requiresAuth(), (request, response) => {
   response.send(JSON.stringify(request.oidc.user));
 });
@@ -23,19 +28,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 //manipular fácilmente las peticiones que llegan en formato JSON
 app.use(bodyParser.json());
 
-// ---- config auth0 ----
-// ESTA CONFIG LA VOY ABORRAR, SOLO ES PARA PROBAR
-const config = {
-  authRequired: false,
-  auth0Logout: true,
-  secret: 'a long, randomly-generated string stored in env',
-  baseURL: 'http://localhost:4000',
-  clientID: 'ebjmTkCRasIP7rZTr94JMv30LylM148G',
-  issuerBaseURL: 'https://dev-fx-x8vee.us.auth0.com'
-};
-
 // auth router attaches /login, /logout, and /callback routes to the baseURL
-app.use(auth(config));
+app.use(auth(authconfig));
 
 // req.isAuthenticated is provided from the auth router
 app.get('/', (request, response) => {
