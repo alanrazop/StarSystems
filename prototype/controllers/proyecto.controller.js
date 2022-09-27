fd58c86e6e06bfa7b755251b2ea6cab6a70d1704
felix-rojas <a01201946@tec.mx>
2022-09-27 12:27:34 -0500

    add query of active projects to view

--- prototype/controllers/proyecto.controller.js
+++ prototype/controllers/proyecto.controller.js
@@ -1,5 +1,15 @@
 const path = require('path');
+const PA = require('../models/proyectos_activos.model')
+
+
 
 exports.getProyecto = (request, response, next) => {
-    response.render(path.join('proyectos.ejs'));
+    PA.fetchAll()
+        .then(([rows, fieldData]) => {
+            console.log(rows[0]);
+            response.render(path.join('proyectos.ejs'), {
+                activos: rows[0]});
+        }).catch(error => {
+            console.log(error);
+        })
 };
