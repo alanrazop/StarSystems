11f4889392da2fd8974697d4e1f7cb32c1f2a82f
felix-rojas <a01201946@tec.mx>
2022-10-03 14:26:16 -0500

    WIP

--- prototype/app.js
+++ prototype/app.js
@@ -1,6 +1,7 @@
 // environment vars
 const authconfig = require('./auth0.env');
 // dependencias
+var reqprom = require("request-promise");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const path = require("path");
@@ -8,31 +9,58 @@ const multer = require('multer');
 // express maneja rutas
 const express = require("express");
 const app = express();
-const { auth, 
-    requiresAuth, 
-    claimEquals,
-    claimIncludes,
-    claimCheck } = require('express-openid-connect');
+const { auth,
+  requiresAuth,
+  claimEquals,
+  claimIncludes,
+  claimCheck } = require('express-openid-connect');
 
+// ------------ AXIOS ------------ //
 const axios = require("axios").default;
-const options = {
+
+const optionsAxios = {
   method: 'POST',
   url: 'https://dev-fx-x8vee.us.auth0.com/oauth/token',
-  headers: {'content-type': 'application/x-www-form-urlencoded'},
-  data: new URLSearchParams({
-    grant_type: 'client_credentials',
-    client_id: 'ebjmTkCRasIP7rZTr94JMv30LylM148G',
-    client_secret: 'dELPyfnxxVy0zoar9J6UbzsmS94L1zDtdasLwfiEsphJFrY4nGIHeGxgxGTDOmnL',
-    audience: 'https://API-test.com'
-  })
+  headers: { 'content-type': 'application/json' },
+  body: '{"client_id":"XtD5sgtJKspUSd6Rh3FEWd2kGe0S01BK","client_secret":"kxMLS93i_cx1Y8aZ8x4z_Z2blIp711BOoHX5fnUXIZ-Uz8Msb38GxxHzHwE113I1","audience":"https://dev-fx-x8vee.us.auth0.com/api/v2/","grant_type":"client_credentials"}'
 };
 
-axios.request(options).then(function (response) {
-  console.log(response.data);
-}).catch(function (error) {
-  console.error(error);
-});
 
+
+async function get_token(options) { 
+  let opt = {
+    method: "GET",
+    url: "https://dev-fx-x8vee.us.auth0.com/api/v2",
+    headers: { "authorization": "" }
+  } 
+  await reqprom(options, function (error, response, body) {
+    if (error) throw new Error(error);})
+      .then((result => {
+        
+  })). catch(error => {
+        console.log(error);
+  });
+}
+
+function post_token(opts) {
+  axios(opts)
+    .then(response => {
+      console.log(response.data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
+
+
+
+console.log('-----------ORA PERRRO CHECATE ESTE CIERRE--------------')
+console.log(get_token(optionsAxios))
+console.log('-------------------------')
+
+
+
+// ------------ AXIOS ------------ //
 // // ---------------------- JSON WEB TOKEN BEGIN --------------------
 // const { expressjwt : jwt } = require('express-jwt');
 // const jwks = require('jwks-rsa');
@@ -56,21 +84,21 @@ axios.request(options).then(function (response) {
 // });
 
 // const jwtCheck = jwt({
-    //       secret: jwks.expressJwtSecret({
-        //           cache: true,
-        //           rateLimit: true,
-        //           jwksRequestsPerMinute: 5,
-        //           jwksUri: 'https://dev-fx-x8vee.us.auth0.com/.well-known/jwks.json'
-        //     }),
-        //     audience: 'http://localhost:4000',
-        //     issuer: 'https://dev-fx-x8vee.us.auth0.com/',
-        //     algorithms: ['RS256']
-        // });
-        
-        // app.use(jwtCheck);
-        
-        // app.get('/authorized', function (req, res) {
-            //     res.send('Secured Resource');
+//       secret: jwks.expressJwtSecret({
+//           cache: true,
+//           rateLimit: true,
+//           jwksRequestsPerMinute: 5,
+//           jwksUri: 'https://dev-fx-x8vee.us.auth0.com/.well-known/jwks.json'
+//     }),
+//     audience: 'http://localhost:4000',
+//     issuer: 'https://dev-fx-x8vee.us.auth0.com/',
+//     algorithms: ['RS256']
+// });
+
+// app.use(jwtCheck);
+
+// app.get('/authorized', function (req, res) {
+//     res.send('Secured Resource');
 // });
 // ---------------------- JSON WEB TOKEN END ----------------------
 
@@ -92,22 +120,22 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 //fileStorage: Es nuestra constante de configuración para manejar el almacenamiento
 const fileStorage = multer.diskStorage({
-    destination: (request, file, callback) => {
-        //'uploads': Es el directorio del servidor donde se subirán los archivos 
-        callback(null, 'uploads');
-    },
-    filename: (request, file, callback) => {
-        //aquí configuramos el nombre que queremos que tenga el archivo en el servidor, 
-        //para que no haya problema si se suben 2 archivos con el mismo nombre concatenamos el timestamp
-        callback(null, Date.now() + '-' +file.originalname);
-    },
+  destination: (request, file, callback) => {
+    //'uploads': Es el directorio del servidor donde se subirán los archivos 
+    callback(null, 'uploads');
+  },
+  filename: (request, file, callback) => {
+    //aquí configuramos el nombre que queremos que tenga el archivo en el servidor, 
+    //para que no haya problema si se suben 2 archivos con el mismo nombre concatenamos el timestamp
+    callback(null, Date.now() + '-' + file.originalname);
+  },
 });
 
 //En el registro, pasamos la constante de configuración y
 //usamos single porque es un sólo archivo el que vamos a subir, 
 //pero hay diferentes opciones si se quieren subir varios archivos. 
 //'archivo' es el nombre del input tipo file de la forma
-app.use(multer({ storage: fileStorage }).single('archivo')); 
+app.use(multer({ storage: fileStorage }).single('archivo'));
 
 
 //manipular fácilmente las peticiones que llegan en formato JSON
@@ -116,9 +144,9 @@ app.use(bodyParser.json());
 // auth router attaches /login, /logout, and /callback routes to the baseURL
 app.use(auth(authconfig));
 
-app.get( '/', claimCheck(({ isAdmin }) => isAdmin),
-    (req, res) => res.send(`Hello ${req.oidc.user.sub}, this is the payroll section.`)
-  );
+app.get('/', (req, res, next) => {
+  res.send(`Hello ${req.oidc.user.sub}, this is the payroll section.`)
+});
 
 // ---- config auth0 ----
 
@@ -129,8 +157,6 @@ const rutas_coord = require("./routes/coord.routes");
 app.use('/lider', requiresAuth(), rutas_lider);
 app.use('/coord', requiresAuth(), rutas_coord);
 app.use('/colab', requiresAuth(), rutas_colab);
-
-
 
 
 app.listen(4000, () => console.log("http://localhost:4000/"));
--- prototype/models/user.model.js
+++ prototype/models/user.model.js
@@ -1,5 +1,18 @@
-const { auth } = require('express-openid-connect');
+const { auth, requiresAuth, claimEquals,claimIncludes,claimCheck } = require('express-openid-connect');
 
 module.exports = class User {
-    request.oidc.user
+    constructor(u_nickname, u_name, u_picture, u_updated_at, u_email, u_email_verified, u_sub, u_sid) {
+        this.nickname = u_nickname,
+        this.name = u_name,
+        this.picture = u_picture,
+        this.updated_at = u_updated_at,
+        this.email = u_email,
+        this.email_verified = u_email_verified
+        this.sub = u_sub,
+        this.sid = u_sid
+    }
+
+    static fetchUserData() {
+        return request.oidc.user;
+    }
 }
