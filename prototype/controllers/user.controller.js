3664d6b4473bd082c00eb3f1e6f0f685e353c83f
felix-rojas <a01201946@tec.mx>
2022-10-03 17:21:08 -0500

    store user data in model

--- prototype/controllers/user.controller.js
+++ prototype/controllers/user.controller.js
@@ -1,9 +1,7 @@
 const User = require("../models/user.model");
 
 exports.getUser  = async (request, response, next) => {
-    const user_data = request.oidc.user;
-    const user_session = user_data.sid;
-    response.render('error.ejs', user_data);
+    const user_data = new User(request.oidc.user);
+    await response.render('error.ejs', user_data);
     console.log(user_data)
-    console.log(user_session)
 }
--- prototype/models/user.model.js
+++ prototype/models/user.model.js
@@ -1,18 +1,18 @@
 const { auth, requiresAuth, claimEquals,claimIncludes,claimCheck } = require('express-openid-connect');
 
 module.exports = class User {
-    constructor(u_nickname, u_name, u_picture, u_updated_at, u_email, u_email_verified, u_sub, u_sid) {
-        this.nickname = u_nickname,
-        this.name = u_name,
-        this.picture = u_picture,
-        this.updated_at = u_updated_at,
-        this.email = u_email,
-        this.email_verified = u_email_verified
-        this.sub = u_sub,
-        this.sid = u_sid
+    constructor(thing) {
+        this.nickname = thing.nickname,
+        this.name = thing.name,
+        this.picture = thing.picture,
+        this.updated_at = thing.updated_at,
+        this.email = thing.email,
+        this.email_verified = thing.email_verified
+        this.sub = thing.sub,
+        this.sid = thing.sid
     }
 
-    static fetchUserData() {
-        return request.oidc.user;
+    static fetchSession() {
+        return this.sid;
     }
 }
