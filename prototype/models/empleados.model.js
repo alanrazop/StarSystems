ac184b1b752bb28bcaa7964dae802566a8491f98
felix-rojas <a01201946@tec.mx>
2022-10-06 14:57:43 -0500

    load inputs adjusted hours

--- prototype/models/empleados.model.js
+++ prototype/models/empleados.model.js
@@ -19,19 +19,13 @@ module.exports = class Empleados {
         return db.execute('SELECT * FROM `empleado` WHERE id_rol IN (1,2)')
     }
 
-    delimiter //
-    CREATE function capactiyCount (OUT cities INT)
-       BEGIN
-         SELECT COUNT(*) INTO cities FROM world.city
-         WHERE CountryCode = country;
-       END//
 
     static fetchTC() {
-        return db.execute('SELECT COUNT() as tiempo_completo FROM empleado e, rol r WHERE e.id_rol = r.id_rol AND is_tiempo_completo="1"');
+        return db.execute('SELECT COUNT(*) as tiempo_completo FROM empleado e, rol r WHERE e.id_rol = r.id_rol AND is_tiempo_completo=1');
     }
 
     static fetchTM() {
-        return db.execute('SELECT () as tiempo_medio FROM empleado e, rol r WHERE e.id_rol = r.id_rol AND is_tiempo_completo="0"');
+        return db.execute('SELECT COUNT(*) as tiempo_medio FROM empleado e, rol r WHERE e.id_rol = r.id_rol AND is_tiempo_completo=0');
     }
 
     // static NombreEmpleadoRegistrables(empleadoSolicitante){
