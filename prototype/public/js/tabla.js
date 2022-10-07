799d92bdc7d5bde91fe61817b0558972561bb541
felix-rojas <a01201946@tec.mx>
2022-10-07 12:30:24 -0500

    parse ALL float outputs to 3 digits

--- prototype/public/js/tabla.js
+++ prototype/public/js/tabla.js
@@ -5,6 +5,11 @@ async function checkAjuste() {
         horas_proyectos.push(parseFloat(element.value))
     });
     const total = horas_proyectos.reduce((a, b) => a + b, 0);
-    console.log(total)
-    let bod = document.getElementById("horas_totales_ajustadas").childNodes[0].data = total.toFixed(2);
+    document.getElementById("horas_totales_ajustadas").childNodes[0].data = total.toFixed(3);
 }
+
+async function capTotal() {
+    let updateValue= document.getElementById("personal_tm").value * document.getElementById("horas_tm").value +
+    (document.getElementById("personal_tc").value * document.getElementById("horas_tc").value);
+    document.getElementById("capacidad_total").value = updateValue.toFixed(3);
+  }
