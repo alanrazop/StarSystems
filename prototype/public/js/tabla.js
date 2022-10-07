b17166bfb81d7725715d54d19971a5410c8394ed
felix-rojas <a01201946@tec.mx>
2022-10-07 18:17:11 -0500

    float format done

--- prototype/public/js/tabla.js
+++ prototype/public/js/tabla.js
@@ -5,11 +5,23 @@ async function checkAjuste() {
         horas_proyectos.push(parseFloat(element.value))
     });
     const total = horas_proyectos.reduce((a, b) => a + b, 0);
-    document.getElementById("horas_totales_ajustadas").childNodes[0].data = total.toFixed(3);
+
+    // check for float 
+    // code adapted from: 
+    // Borislav Hadzhiev @ https://bobbyhadz.com/blog/javascript-check-if-value-is-float
+    if (typeof(total) === 'number' && Number.isInteger(total)) {
+        document.getElementById("horas_totales_ajustadas").childNodes[0].data = total;
+    } else {
+        document.getElementById("horas_totales_ajustadas").childNodes[0].data = total.toFixed(3);
+    }
 }
 
 async function capTotal() {
-    let updateValue= document.getElementById("personal_tm").value * document.getElementById("horas_tm").value +
+    const updatedValue = (document.getElementById("personal_tm").value * document.getElementById("horas_tm").value) +
     (document.getElementById("personal_tc").value * document.getElementById("horas_tc").value);
-    document.getElementById("capacidad_total").value = updateValue.toFixed(3);
+    if (typeof(updatedValue) === 'number' && Number.isInteger(updatedValue)) {
+        document.getElementById("capacidad_total").value = updatedValue;
+    } else {
+        document.getElementById("capacidad_total").value = updatedValue.toFixed(3);
+    }
   }
